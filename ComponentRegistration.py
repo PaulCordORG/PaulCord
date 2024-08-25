63d3df4651bd8150484f752eaeeff69551423503
Paweł Brzyś <53524761+Pawelek-dev@users.noreply.github.com>
2024-08-25 22:20:05 +0200

    Delete ComponentRegistration.py

--- ComponentRegistration.py
+++
@@ -1,6 +0,0 @@
-def register_component_handlers(client):
-    for attr_name in dir(client):
-        attr = getattr(client, attr_name)
-        if callable(attr) and hasattr(attr, '_component_handler_info'):
-            custom_id = attr._component_handler_info
-            client.component_handlers[custom_id] = attr
